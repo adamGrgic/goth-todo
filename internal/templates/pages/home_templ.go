bdc4b2214a1ebcd21c3b2b7b9263b4a8a15d88af
adamgrgic <argrgic@gmail.com>
2025-03-17 09:18:52 -0600

    test

--- internal/templates/pages/home_templ.go
+++ internal/templates/pages/home_templ.go
@@ -29,7 +29,7 @@ func Home() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<button class=\"btn btn-primary\">foo</button>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<section><div class=\"container\" style=\"min-height: 800px;\">This is a home page</div></section>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
